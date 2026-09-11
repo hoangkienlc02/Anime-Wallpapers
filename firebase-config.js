fac544ce6106b66c2a68f5fa283f31945a84be1f
hoangkienlc02 <kienlc2k2@gmail.com>
2026-09-12 00:14:11 +0700

    fix Firebase project configuration

--- firebase-config.js
+++ firebase-config.js
@@ -4,14 +4,17 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 
 // These values identify the Firebase project; they are not server secrets.
 // Access is protected by Firebase Authentication and Firestore Rules.
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAKYazmv5LhCsRUlGRoYm5RSHKuV5nT24A",
-    authDomain: "images-web-8e8a0.firebaseapp.com",
-    projectId: "images-web-8e8a0",
-    storageBucket: "images-web-8e8a0.firebasestorage.app",
-    messagingSenderId: "962206114668",
-    appId: "1:962206114668:web:2178cd8a304abaddce8949"
+  apiKey: "AIzaSyAKYazmv5LhCsRUlgRoYm5RSHKuV5nT24A",
+  authDomain: "images-web-8e8a0.firebaseapp.com",
+  projectId: "images-web-8e8a0",
+  storageBucket: "images-web-8e8a0.firebasestorage.app",
+  messagingSenderId: "962206114668",
+  appId: "1:962206114668:web:2178cd8a304abaddce8949",
+  measurementId: "G-Q0XZ0TXTWM"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
